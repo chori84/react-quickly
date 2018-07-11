d0927e7adb969ca609b2d3b9982db198a592105e
Ubuntu <ubuntu@ip-172-31-22-163.ap-northeast-2.compute.internal>
2018-07-11 02:36:35 +0000

    ch03-JSX

---
+++ jsx/HelloWorld.jsx
@@ -0,0 +1,23 @@
+class HelloWorld extends React.Component {
+  render() {
+    return <h1 {...this.props}>Hello {this.props.frameworkName} world!!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <HelloWorld
+      id='ember'
+      frameworkNanem='Ember.js'
+      title='A framework for creating ambitious web applications.' />
+    <HelloWorld
+      id='backbone'
+      frameworkNanem='Backbone.js'
+      title='Backbone.js gives structure to web applications...' />
+    <HelloWorld
+      id='angular'
+      frameworkNanem='Angular.js'
+      title='SUperheroic JavaScript MVW Framework' />
+  </div>,
+  document.getElementById('content')
+);
