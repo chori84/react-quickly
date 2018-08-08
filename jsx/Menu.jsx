76e030e37a90fe7c555b8aca47b98b4a1fb9f2aa
HeeChul Choi <slivers721@gmail.com>
2018-08-08 04:44:10 +0000

    ch09-프로젝트: Menu 컴포넌트

---
+++ jsx/Menu.jsx
@@ -0,0 +1,22 @@
+class Menu extends React.Component {
+  render() {
+    let menus = [
+      'Home',
+      'About',
+      'Services',
+      'Portfolio',
+      'Contact us'
+    ];
+    
+    return <div>
+      {menus.map((v, i) => {
+        return <div key={i}><Link label={v} /></div>;
+      })}
+    </div>;
+  }
+}
+
+ReactDOM.render(
+  <Menu />,
+  document.getElementById('menuContent')
+);
